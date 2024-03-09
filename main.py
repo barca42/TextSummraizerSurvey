c3e5cdf7b4ab6cc4a5b054e56dd78ea75794b649
barca42 <barboraivanova42@gmail.com>
2024-03-09 21:06:11 +0000

    Randomly choose questions + choice.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import flask
 from flask import Flask, render_template
+import pandas as pd
+
+questions = pd.read_csv('data/questions.csv')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return render_template('survey.html')
+    sample_questions = questions.sample(3)
+
+    return render_template('survey.html', questions=sample_questions.to_dict('records'))
