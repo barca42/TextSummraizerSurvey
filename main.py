5367986e80fdf89d5762b0241b81e470c122520d
barca42 <barboraivanova42@gmail.com>
2024-03-17 13:33:17 +0000

    sample with probabilities

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import pandas as pd
 from database import db, Answer, AnswerType
 from uuid import uuid4
+import numpy
+
+
 
 
 questions = pd.read_csv('data/questions.csv')
@@ -17,8 +20,13 @@ with app.app_context():
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
     if request.method == "GET":
-        sample_questions = questions.sample(4)
-        return render_template("survey.html", questions=sample_questions.to_dict('records'))
+        result = db.session.query(Answer.question_id, db.func.count()).group_by(Answer.question_id)
+        answer_counts = dict(result)
+        answer_counts = {key: answer_counts.get(key, 0) for key in range(1, 16)}
+
+        question_sample = sample_questions(answer_counts)
+
+        return render_template("survey.html", questions=question_sample.to_dict('records'))
     if request.method == "POST":
         user_id = str(uuid4())
         for key,value in request.form.items():
@@ -28,6 +36,18 @@ def hello_world():
 
         db.session.commit()
         return render_template("thankyou.html")
+
+
+def sample_questions(answer_counts):
+    print(answer_counts)
+    maximum = max(answer_counts.values())
+    for key in answer_counts:
+        answer_counts[key] = 2 ** (maximum - answer_counts[key])
+    total = sum(answer_counts.values())
+    probabilities = [value / total for value in list(answer_counts.values())]
+    question_numbers = (numpy.random.choice(a=list(answer_counts.keys()), size=4, replace=False, p=probabilities))
+
+    return question_numbers
 
 
 if __name__ == "__main__":
